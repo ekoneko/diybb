4bd000376e4d12b8b77beaf356c43e27c4ec69c9
ekoneko <ekoneko9@gmail.com>
2014-06-13 00:33:48 +0800

    tinymce-yentext

--- public/user_libraries/tinymce/editor.js
+++ public/user_libraries/tinymce/editor.js
@@ -16,24 +16,31 @@
             };
 
         tinymce.baseURL = '/libraries/tinymce/js/tinymce';
-        
+
         mode = mode || 'standard';
         switch (mode) {
         case 'standard':
             modeOptions = {
-                toolbar1 : 'undo redo | styleselect bold italic forecolor outdent indent | link unlink | image code',
+                toolbar1 : 'undo redo | styleselect bold italic forecolor outdent indent | link unlink | image code yen',
                 plugins : 'link code textcolor',
                 external_plugins : {
-                    image : '/user_libraries/tinymce/plugins/image/plugin.js'
+                    image : '/user_libraries/tinymce/plugins/image/plugin.js',
+                    yen : '/libraries/tinymce-yentext/plugin.min.js'
                 }
             };
+            tinymce.DOM.loadCSS('/libraries/tinymce-yentext/style.css');
             break;
         case 'tiny':
             modeOptions = {
-                toolbar1 : 'bold italic forecolor | link unlink',
+                toolbar1 : 'bold italic forecolor | link unlink | yen',
                 plugins : 'link textcolor',
-                height : 100
+                statusbar: false,
+                height : 100,
+                external_plugins : {
+                    yen : '/libraries/tinymce-yentext/plugin.min.js'
+                }
             };
+            tinymce.DOM.loadCSS('/libraries/tinymce-yentext/style.css');
             break;
         }
         tinymce.extend(options, modeOptions);
