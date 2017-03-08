d57d5a469fc3ec12dc1d79cccd5ef2167277adb6
ekoneko <9@ikaze.me>
2017-03-09 07:59:26 +0800

    style: comment info

--- www/code/components/Common/Comment/Item.jsx
+++ www/code/components/Common/Comment/Item.jsx
@@ -7,6 +7,10 @@ export default class Item extends React.PureComponent {
     return (
       <div className="comment-item">
         <Avatar size={30} sharp="circle"></Avatar>
+        <div className="info">
+          <span className="tag">kaze</span>
+          <span className="tag">2017-03-09</span>
+        </div>
         <div className="text">some comment.</div>
       </div>
     )
